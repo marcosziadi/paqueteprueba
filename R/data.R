b0ba9874753ccf761172d6e82ee509fb1b0097db
marcosziadi <139mziadi@gmail.com>
2024-10-04 10:02:44 -0300

    Agrego documentacion del conjunto de datos

---
+++ R/data.R
@@ -0,0 +1,13 @@
+#' Conjunto de datos de ejemplo
+#'
+#' Los datos son utilizados como ejemplo de nuestra funcion de suma.
+#'
+#' Estos son detalles extras de la funcion.
+#'
+#' @format los datos son generados de forma sintética.
+#' \describe{
+#'  \item{a}{muestra de datos}
+#'  \item{b}{muestra de datos multiplicada por cinco}
+#' }
+#' @source generacion propia con funciones de R
+"datos"
---
+++ data_raw/crea_datos.R
@@ -0,0 +1,10 @@
+#Leer los datos
+#Limpiar los datos
+#Manipulación de datos
+
+datos <- data.frame(a = sample(1000),
+                    b = sample(1000) *5)
+
+usethis::use_data(datos, overwrite = TRUE)
+
+
